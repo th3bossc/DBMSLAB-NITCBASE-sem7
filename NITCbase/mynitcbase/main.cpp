bb1d1e3175529c92a88c40d756782fa77f88f880
th3bossc <diljith2003@gmail.com>
2024-07-23 12:00:11 +0530

    stage 1 done with exercise

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,6 +2,13 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+
+void copyArray(int* dest, unsigned char* src, int len) {
+  for (int i = 0; i < len; i++)
+    dest[i] = (int)src[i];
+}
+
 
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
@@ -9,5 +16,22 @@ int main(int argc, char *argv[]) {
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
+  int blockAllocationMap[4*BLOCK_SIZE];
+
+  for (int i = 0; i < 4; i++) {
+    unsigned char buffer[BLOCK_SIZE];
+    
+    Disk::readBlock(buffer, i);
+
+    copyArray(blockAllocationMap+i*BLOCK_SIZE, buffer, BLOCK_SIZE);
+
+  }
+  for (int i = 0; i < 100; i++) {
+    std::cout << i << " ";
+    std::cout << ((blockAllocationMap[i] == 4) 
+      ? "BMAP"
+      : "UNUSED BLOCK")
+    << std::endl;
+  }
   return FrontendInterface::handleFrontend(argc, argv);
 }
