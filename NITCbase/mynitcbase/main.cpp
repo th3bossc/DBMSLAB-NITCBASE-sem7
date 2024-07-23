bcb05cd022cbc26b092a4e41250cd345a4243e9b
th3bossc <diljith2003@gmail.com>
2024-07-23 19:08:07 +0530

    stage 2 first exercise

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,36 +2,51 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
-#include <iostream>
 
-void copyArray(int* dest, unsigned char* src, int len) {
-  for (int i = 0; i < len; i++)
-    dest[i] = (int)src[i];
+
+void printAllRelationsAndAttributes() {
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  HeadInfo relCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+
+  for (int i = 0; i < relCatHeader.numEntries; i++) {
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    int attrCatBlockNumber = ATTRCAT_BLOCK;
+    while (attrCatBlockNumber != -1) {
+      RecBuffer attrCatBuffer(attrCatBlockNumber);
+      HeadInfo attrCatHeader;
+      attrCatBuffer.getHeader(&attrCatHeader);
+      attrCatBlockNumber = attrCatHeader.rblock;
+
+
+      for (int j = 0; j < attrCatHeader.numEntries; j++) {
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal))
+          continue;
+          
+
+        const char* attrType = (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER)
+          ? "NUM"
+          : "STR";
+
+        printf("%s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    } 
+    printf("\n");
+  }
 }
-
 
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
+  printAllRelationsAndAttributes();
 
-  int blockAllocationMap[4*BLOCK_SIZE];
-
-  for (int i = 0; i < 4; i++) {
-    unsigned char buffer[BLOCK_SIZE];
-    
-    Disk::readBlock(buffer, i);
-
-    copyArray(blockAllocationMap+i*BLOCK_SIZE, buffer, BLOCK_SIZE);
-
-  }
-  for (int i = 0; i < 100; i++) {
-    std::cout << i << " ";
-    std::cout << ((blockAllocationMap[i] == 4) 
-      ? "BMAP"
-      : "UNUSED BLOCK")
-    << std::endl;
-  }
   return FrontendInterface::handleFrontend(argc, argv);
 }
