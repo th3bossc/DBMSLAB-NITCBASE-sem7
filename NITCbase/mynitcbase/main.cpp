d2f0bd199ae6e574ed166a582ef5184fee402915
th3bossc <diljith2003@gmail.com>
2024-07-29 01:08:18 +0530

    put back code for exercise stage 3

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -22,14 +22,80 @@ void printRelAttr() {
   }
 }
 
+void printAllRelationsAndAttributes() {
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  HeadInfo relCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+
+  for (int i = 0; i < relCatHeader.numEntries; i++) {
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    int attrCatBlockNumber = ATTRCAT_BLOCK;
+    while (attrCatBlockNumber != -1) {
+      RecBuffer attrCatBuffer(attrCatBlockNumber);
+      HeadInfo attrCatHeader;
+      attrCatBuffer.getHeader(&attrCatHeader);
+      attrCatBlockNumber = attrCatHeader.rblock;
+
+
+      for (int j = 0; j < attrCatHeader.numEntries; j++) {
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal))
+          continue;
+
+
+        const char* attrType = (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER)
+          ? "NUM"
+          : "STR";
+
+        printf("%s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    } 
+    printf("\n");
+  }
+}
+
+
+// rename class attr to batch and print all 
+void exercise2() {
+  const char targetRelation[] = "Students";
+  int attrCatBlockNumber = ATTRCAT_BLOCK;
+  while (attrCatBlockNumber != -1) {
+    RecBuffer attrCatBuffer(attrCatBlockNumber);
+    HeadInfo attrCatHeader;
+    attrCatBuffer.getHeader(&attrCatHeader);
+    attrCatBlockNumber = attrCatHeader.rblock;
+    for (int j = 0; j < attrCatHeader.numEntries; j++) {
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, targetRelation) == 0) {
+        if (strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Class") == 0) {
+          unsigned char buffer[BLOCK_SIZE];
+          Disk::readBlock(buffer, ATTRCAT_BLOCK);
+          memcpy(buffer + 52 + 96*j + 16, "Batch", ATTR_SIZE);
+          Disk::writeBlock(buffer, ATTRCAT_BLOCK);
+          printAllRelationsAndAttributes();
+        }
+      }
+    }
+  }
+}
+
+
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   Disk disk_run;
   StaticBuffer buffer;
   OpenRelTable cache;
 
-  printRelAttr();
-  // exercise2();
+  // printRelAttr();
+  exercise2();
   // printAllRelationsAndAttributes();
 
   return FrontendInterface::handleFrontend(argc, argv);
