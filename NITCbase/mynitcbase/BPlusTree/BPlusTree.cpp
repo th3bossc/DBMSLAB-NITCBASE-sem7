658d855f74a024d4e3ce6ca772db3d192470251a
th3bossc <diljith2003@gmail.com>
2024-08-16 16:53:40 +0530

    [FIX]: in BPlusTree::bPlusSearch() compareAttrs()

--- NITCbase/mynitcbase/BPlusTree/BPlusTree.cpp
+++ NITCbase/mynitcbase/BPlusTree/BPlusTree.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 
 RecId BPlusTree::bPlusSearch(int relId, char attrName[ATTR_SIZE], union Attribute attrVal, int op) {
+    
     IndexId searchIndex;
     AttrCacheTable::getSearchIndex(relId, attrName, &searchIndex);
 
@@ -13,14 +14,14 @@ RecId BPlusTree::bPlusSearch(int relId, char attrName[ATTR_SIZE], union Attribut
     if (searchIndex.block == -1 || searchIndex.index == -1) {
         block = attrCatEntry.rootBlock;
         index = 0;
-
-        if (block == -1)
+    
+        if (block == -1) 
             return RecId({-1, -1});
     }
     else {
         block = searchIndex.block;
-        index = searchIndex.index + 1;
-
+        index = searchIndex.index+1;
+    
         IndLeaf leaf(block);
 
         HeadInfo leafHead;
@@ -29,57 +30,44 @@ RecId BPlusTree::bPlusSearch(int relId, char attrName[ATTR_SIZE], union Attribut
         if (index >= leafHead.numEntries) {
             block = leafHead.rblock;
             index = 0;
-            if (block == -1) {
+
+            if (block == -1)
                 return RecId({-1, -1});
-            }
         }
     }
 
-    while (StaticBuffer::getStaticBlockType(block) == IND_INTERNAL) {
-        IndInternal internalBlock(block);
+    while(StaticBuffer::getStaticBlockType(block) == IND_INTERNAL) {
 
+        IndInternal internalBlock(block);
+    
         HeadInfo intHead;
         internalBlock.getHeader(&intHead);
 
         InternalEntry intEntry;
-        internalBlock.getEntry(&intEntry, 0);
 
-        if (
-            op == NE || 
-            op == LT ||
-            op == LE
-        ) {
+        if (op == NE || op == LT || op == LE) {
+            internalBlock.getEntry(&intEntry, 0);
             block = intEntry.lChild;
         }
         else {
             int targetIndex = -1;
-            for (int i = 1; i < intHead.numEntries; i++) {
-                InternalEntry currentIntEntry;
-                internalBlock.getEntry(&currentIntEntry, i);
+            for (int i = 0; i < intHead.numEntries; i++) {
+                internalBlock.getEntry(&intEntry, i);
+                int cmpVal = compareAttrs(intEntry.attrVal, attrVal, attrCatEntry.attrType);
 
-                int cmpVal = compareAttrs(currentIntEntry.attrVal, attrVal, attrCatEntry.attrType);
-
-                if (
-                    (op == EQ && cmpVal == 0)   ||
-                    (op == GT && cmpVal > 0)    ||
-                    (op == GE && cmpVal >= 0)
-                ) {
+                if (cmpVal >= 0) {
                     targetIndex = i;
                     break;
                 }
             }
-
-            if (targetIndex != -1) {
-                InternalEntry targetInternalEntry;
-                internalBlock.getEntry(&targetInternalEntry, targetIndex);
-
-                block = targetInternalEntry.lChild;
+    
+            if (targetIndex == -1) {
+                internalBlock.getEntry(&intEntry, intHead.numEntries-1);
+                block = intEntry.rChild;
             }
             else {
-                InternalEntry targetInternalEntry;
-                internalBlock.getEntry(&targetInternalEntry, intHead.numEntries-1);
-
-                block = targetInternalEntry.rChild;
+                internalBlock.getEntry(&intEntry, targetIndex);
+                block = intEntry.lChild;
             }
         }
     }
@@ -91,11 +79,10 @@ RecId BPlusTree::bPlusSearch(int relId, char attrName[ATTR_SIZE], union Attribut
         leafBlock.getHeader(&leafHead);
 
         Index leafEntry;
-        leafBlock.getEntry(&leafEntry, index);
-
-        int cmpVal = compareAttrs(leafEntry.attrVal, attrVal, attrCatEntry.attrType);
-
         while (index < leafHead.numEntries) {
+            leafBlock.getEntry(&leafEntry, index);
+            int cmpVal = compareAttrs(leafEntry.attrVal, attrVal, attrCatEntry.attrType);
+
             if (
                 (op == EQ && cmpVal == 0)   ||
                 (op == LE && cmpVal <= 0)   ||
@@ -104,22 +91,19 @@ RecId BPlusTree::bPlusSearch(int relId, char attrName[ATTR_SIZE], union Attribut
                 (op == GE && cmpVal >= 0)   ||
                 (op == NE && cmpVal != 0)
             ) {
-                searchIndex.block = block;
-                searchIndex.index = index;
-
+                searchIndex = {block, index};
                 AttrCacheTable::setSearchIndex(relId, attrName, &searchIndex);
-
                 return RecId({leafEntry.block, leafEntry.slot});
             }
-            else if(
-                (op == EQ || op == LE || op == LT) && cmpVal > 0
+            else if (
+                (op == EQ || op == LE || op == LT)  &&
+                cmpVal > 0
             ) {
                 return RecId({-1, -1});
             }
 
             index++;
         }
-
         if (op != NE)
             break;
 
